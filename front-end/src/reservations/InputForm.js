64b8fe65c04c9d790eec984a99c0dfc6e369861f
Mary Benjamin <marybenjamin713@gmail.com>
2022-05-09 21:51:37 -0500

    random things

--- front-end/src/reservations/InputForm.js
+++ front-end/src/reservations/InputForm.js
@@ -100,8 +100,8 @@ const ac = new AbortController()
           <input
             className="form-control"
             type="text"
-            id="first_name"
             name="first_name"
+            id="first_name"
             required={true}
             onChange={handleChange}
             value={formData.first_name}
@@ -111,8 +111,8 @@ const ac = new AbortController()
         <input
           className="form-control"
           type="text"
-          id="last_name"
           name="last_name"
+          id="last_name"
           required={true}
           onChange={handleChange}
           value={formData.last_name}
@@ -122,8 +122,8 @@ const ac = new AbortController()
           <input
             className="form-control"
             type="tel"
-            id="mobile_number"
             name="mobile_number"
+            id="mobile_number"
             required={true}
             placeholder="000-000-0000"
             // pattern="([0-9]{3}-)?[0-9]{3}-[0-9]{4}"
@@ -136,8 +136,8 @@ const ac = new AbortController()
           <input
             className="form-control"
             type="date"
-            id="reservation_date"
             name="reservation_date"
+            id="reservation_date"
             // pattern="\d{4}-\d{2}-\d{2}"
             required={true}
             onChange={handleChange}
@@ -149,8 +149,8 @@ const ac = new AbortController()
           <input
             className="form-control"
             type="time"
-            id="reservation_time"
             name="reservation_time"
+            id="reservation_time"
             // pattern="[0-9]{2}:[0-9]{2}"
             required={true}
             onChange={handleChange}
@@ -162,8 +162,8 @@ const ac = new AbortController()
           <input
             className="form-control"
             type="number"
-            id="people"
             name="people"
+            id="people"
             min={1}
             required={true}
             onChange={handleChangeNum}
